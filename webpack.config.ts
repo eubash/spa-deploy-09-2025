ebbff3feedf54d43300a03c0927d45cdf7fd394b
Evgeniya Bashieva <eugenia.bashieva@remago.com>
2025-10-02 19:56:39 +0200

    add: webpack config for public path

--- webpack.config.ts
+++ webpack.config.ts
@@ -18,7 +18,7 @@ const config: webpack.Configuration = {
     environment: {
       arrowFunction: false,
     },
-    publicPath: '/',
+    publicPath: NODE_ENV === "production" ? PREFIX : '/',
   },
   resolve: {
     extensions: [".js", ".ts"],
